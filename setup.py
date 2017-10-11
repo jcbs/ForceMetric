9f854744b64760b99f8d832ff82241475963f8be
Jacob Seifert <jacob.seifert@gmx.net>
2017-10-11 15:52:18 +0100

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import sys
 
 sys.path.append('/home/jacob/Studies/Oxford/Programming/Python/ForceMetric/')
 
+cythonize("ForceMetric.pyx")
+
 setup(
-    ext_modules=cythonize("ForceMetric.pyx")
+    name='ForceMetric',
+    version='0.1',
+    description='AFM package for Asylum Research .ibw files',
+    author='Jacob Seifert',
+    author_email='jacob.seifert@gmx.net',
+    url='https://github.com/jcbs/ForceMetric',
+    py_modules=['Tools', 'ContactPointDetermination'],
+    ext_modules=[Extension('ForceMetric', ['ForceMetric.c'])]
 )
