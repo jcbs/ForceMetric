ce52bf9d41f1239a33c58368e2a9f1c9b13e99c7
Jacob Seifert <jacob.seifert@gmx.net>
2018-06-11 14:10:23 +0100

    PyPi initialization

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import sys
+import setuptools
+# from distutils.core import setup, Extension
+# from Cython.Build import cythonize
+# import sys
 
-sys.path.append('/home/jacob/Studies/Oxford/Programming/Python/ForceMetric/')
+# sys.path.append('/home/jacob/Studies/Oxford/Programming/Python/ForceMetric/')
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-cythonize("ForceMetric.pyx")
+# cythonize("ForceMetric.pyx")
 
-setup(
+setuptools.setup(
     name='ForceMetric',
-    version='0.1',
+    version='0.0.1',
     description='AFM package for Asylum Research .ibw files',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jacob Seifert',
     author_email='jacob.seifert@gmx.net',
     url='https://github.com/jcbs/ForceMetric',
-    py_modules=['Tools', 'ContactPointDetermination'],
-    ext_modules=[Extension('ForceMetric', ['ForceMetric.c'])]
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programmin Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+        # py_modules=['Tools', 'ContactPointDetermination'],
+        # ext_modules=[Extension('ForceMetric', ['ForceMetric.c'])]
 )
